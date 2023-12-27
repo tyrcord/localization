0d2850cae70ec9bd24e1cd5b32060a3f89839a99
Thibault Zanini <thibaultzanini@gmail.com>
2023-12-27 09:35:54 +0100

    feat: update format script

--- scripts/format.js
+++ scripts/format.js
@@ -13,8 +13,45 @@ function deepSearchJSONFiles(directory) {
     if (stats.isDirectory()) {
       deepSearchJSONFiles(filePath);
     } else if (path.extname(file) === ".json") {
-      sortAndRenameJSONFile(filePath);
+      processAndSortJSONFile(filePath);
     }
+  }
+}
+
+function processAndSortJSONFile(filePath) {
+  try {
+    const fileName = path.basename(filePath);
+
+    // Check if the file is zh_Hans.json
+    if (fileName === "zh_Hans.json") {
+      const newFilePath = path.join(path.dirname(filePath), "zh.json");
+
+      // Rename the file
+      fs.renameSync(filePath, newFilePath);
+      filePath = newFilePath; // Update the filePath to the renamed file
+      console.log(`Renamed file: ${filePath}`);
+    }
+
+    // Read the JSON data from the file
+    const data = fs.readFileSync(filePath, "utf8");
+    let jsonData = JSON.parse(data);
+
+    // Replace spaces before punctuation
+    jsonData = replaceSpacesBeforePunctuation(jsonData);
+
+    // Capitalize first letter of sentences
+    jsonData = capitalizeFirstLetterOfSentences(jsonData);
+
+    // Sort keys alphabetically
+    jsonData = sortKeysAlphabetically(jsonData);
+
+    // Write the sorted JSON data to the file
+    jsonData = JSON.stringify(jsonData, null, 2) + "\n";
+    fs.writeFileSync(filePath, sortedData, "utf8");
+
+    console.log(`Sorted keys in file: ${filePath}`);
+  } catch (error) {
+    console.error(`Error sorting or renaming JSON file: ${filePath}\n`, error);
   }
 }
 
@@ -42,33 +79,31 @@ function replaceSpacesBeforePunctuation(jsonData) {
   return updatedObject;
 }
 
-function sortAndRenameJSONFile(filePath) {
-  try {
-    const fileName = path.basename(filePath);
+function capitalizeFirstLetterOfSentences(jsonData) {
+  const sentenceBoundaryRegex = /(\.|\?|!)\s+([a-z])/g;
 
-    // Check if the file is zh_Hans.json
-    if (fileName === "zh_Hans.json") {
-      const newFilePath = path.join(path.dirname(filePath), "zh.json");
-
-      // Rename the file
-      fs.renameSync(filePath, newFilePath);
-      filePath = newFilePath; // Update the filePath to the renamed file
-      console.log(`Renamed file: ${filePath}`);
-    }
-
-    const data = fs.readFileSync(filePath, "utf8");
-    let jsonData = JSON.parse(data);
-
-    jsonData = replaceSpacesBeforePunctuation(jsonData);
-
-    const sortedJsonData = sortKeysAlphabetically(jsonData);
-    const sortedData = JSON.stringify(sortedJsonData, null, 2) + "\n";
-
-    fs.writeFileSync(filePath, sortedData, "utf8");
-    console.log(`Sorted keys in file: ${filePath}`);
-  } catch (error) {
-    console.error(`Error sorting or renaming JSON file: ${filePath}\n`, error);
+  if (typeof jsonData === "string") {
+    return jsonData.replace(
+      sentenceBoundaryRegex,
+      (match, p1, p2) => p1 + " " + p2.toUpperCase()
+    );
   }
+
+  if (typeof jsonData !== "object" || jsonData === null) {
+    return jsonData;
+  }
+
+  if (Array.isArray(jsonData)) {
+    return jsonData.map(capitalizeFirstLetterOfSentences);
+  }
+
+  const updatedObject = {};
+
+  for (const key in jsonData) {
+    updatedObject[key] = capitalizeFirstLetterOfSentences(jsonData[key]);
+  }
+
+  return updatedObject;
 }
 
 function sortKeysAlphabetically(jsonData) {
